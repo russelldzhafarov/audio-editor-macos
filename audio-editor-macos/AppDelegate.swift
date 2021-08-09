70361e128afc2a2332e05402c487b87909a0df63
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-09 22:55:07 +0800

    Changed default appearance to dark aqua

--- audio-editor-macos/AppDelegate.swift
+++ audio-editor-macos/AppDelegate.swift
@@ -9,17 +9,7 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.appearance = NSAppearance(named: .darkAqua)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
