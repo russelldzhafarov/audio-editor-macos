7812724fed44248f2e013066088520620468ceea
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-11 14:27:58 +0800

    Fixed waveform drawing

--- audio-editor-macos/WaveformView.swift
+++ audio-editor-macos/WaveformView.swift
@@ -9,10 +9,10 @@ import Cocoa
 
 extension NSColor {
     static var waveformColor: NSColor {
-        NSColor(red: 178.0/255.0, green: 199.0/255.0, blue: 233.0/255.0, alpha: 1.0)
+        NSColor(red: 65.0/255.0, green: 167.0/255.0, blue: 208.0/255.0, alpha: 1.0)
     }
     static var waveformBackgroundColor: NSColor {
-        NSColor(red: 65.0/255.0, green: 115.0/255.0, blue: 167.0/255.0, alpha: 1.0)
+        NSColor(red: 30.0/255.0, green: 31.0/255.0, blue: 40.0/255.0, alpha: 1.0)
     }
 }
 
@@ -29,35 +29,45 @@ class WaveformView: NSView {
     // MARK: - Drawing
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
+        
+        // Drawing code here.
         guard let viewModel = viewModel,
               let ctx = NSGraphicsContext.current?.cgContext else { return }
+        
+        let startTime = viewModel.visibleTimeRange.lowerBound
+        let endTime = viewModel.visibleTimeRange.upperBound
+        
+        let duration = endTime - startTime
+        let pxPerSec = bounds.width / CGFloat(duration)
+        
+        let frame = CGRect(x: CGFloat(-startTime) * pxPerSec,
+                           y: CGFloat(0),
+                           width: CGFloat(viewModel.duration) * pxPerSec,
+                           height: bounds.height)
         
         ctx.setFillColor(NSColor.waveformBackgroundColor.cgColor)
         ctx.fill(bounds)
         
-        // Drawing code here.
-        let lineWidth = CGFloat(1.5)
-        let stepInPx = CGFloat(3)
+        let lineWidth = CGFloat(1)
+        let stepInPx = CGFloat(1)
         
-        let koeff = bounds.width / stepInPx
+        let koeff = frame.width / stepInPx
         let stepInSec = viewModel.duration / Double(koeff)
         
         guard stepInSec > 0 else { return }
         
-        var x = CGFloat(0)
+        var x = frame.origin.x
         for time in stride(from: 0.0, to: viewModel.duration, by: stepInSec) {
-            guard time < viewModel.duration else { continue }
-            
             let power = viewModel.power(at: time)
             
             let heigth = max(CGFloat(1),
-                             CGFloat(power) * (bounds.height/2))
+                             CGFloat(power) * (frame.height/2))
             
             ctx.move(to: CGPoint(x: x,
-                                 y: bounds.midY + heigth))
+                                 y: frame.midY + heigth))
             
             ctx.addLine(to: CGPoint(x: x,
-                                    y: bounds.midY - heigth))
+                                    y: frame.midY - heigth))
             
             x += stepInPx
         }
